794b26055a1014e45386af57275d1db36a972a2a
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2025-04-27 17:22:08 +0600

    feat(auth): add firebase configuration and auth context

---
+++ config/firebase.ts
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { getFirestore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCfXZgZmKTPUG4-OwbM3aIA2sWbGjv9UFI',
+  authDomain: 'expense-tracker-app-479e9.firebaseapp.com',
+  projectId: 'expense-tracker-app-479e9',
+  storageBucket: 'expense-tracker-app-479e9.firebasestorage.app',
+  messagingSenderId: '808537211593',
+  appId: '1:808537211593:web:d29f92979c46db3ceb0318'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+// auth
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+})
+
+// db
+export const firestore = getFirestore(app)
