bd1f671e5f77074e33e509335c2e2959266a17be
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2025-04-27 10:05:09 +0600

    feat(welcome): add navigation to login and register screens

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -7,13 +7,17 @@ import { verticalScale } from '@/utils/styling'
 import { colors } from '@/constants/theme'
 import Button from '../../components/Button'
 import Animated, { FadeIn, FadeInDown } from 'react-native-reanimated'
+import { useRouter } from 'expo-router'
 
 const Welcome = () => {
+  const router = useRouter()
   return (
     <ScreenWrapper>
       <View style={styles.container}>
         <View>
-          <TouchableOpacity style={styles.loginButton}>
+          <TouchableOpacity
+            onPress={() => router.push('/(auth)/login')}
+            style={styles.loginButton}>
             <Typo fontWeight={'500'}>Sign in</Typo>
           </TouchableOpacity>
 
@@ -57,7 +61,7 @@ const Welcome = () => {
               .springify()
               .damping(12)}
             style={styles.buttonContainer}>
-            <Button>
+            <Button onPress={() => router.push('/(auth)/register')}>
               <Typo size={22} color={colors.neutral900} fontWeight='600'>
                 Get Started
               </Typo>
