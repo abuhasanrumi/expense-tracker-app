69813938be4f5d894bf2a55bcfb0abcb3be20b41
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2025-04-29 01:14:36 +0600

    feat(tabs): replace default tab bar with custom component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,17 @@
 import { Tabs } from 'expo-router'
 import React from 'react'
 import { StyleSheet } from 'react-native'
+import CustomTabs from '../../components/CustomTabs'
 
 const _layout = () => {
-  return <Tabs></Tabs>
+  return (
+    <Tabs tabBar={CustomTabs} screenOptions={{ headerShown: false }}>
+      <Tabs.Screen name='index' />
+      <Tabs.Screen name='statistics' />
+      <Tabs.Screen name='wallet' />
+      <Tabs.Screen name='profile' />
+    </Tabs>
+  )
 }
 
 export default _layout
