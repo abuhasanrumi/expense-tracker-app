4189ecb160f4d8c143ab9cf0bc58df2de6676d3e
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2025-04-30 02:53:10 +0600

    feat(layout): add profile modal to stack navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
 import { Stack } from 'expo-router'
+import React from 'react'
+import { StyleSheet } from 'react-native'
 import { AuthProvider } from '../contexts/authContext'
 
 const StackLayout = () => {
-  return <Stack screenOptions={{ headerShown: false }}></Stack>
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen
+        name='(modals)/profileModal'
+        options={{
+          presentation: 'modal'
+        }}
+      />
+    </Stack>
+  )
 }
 
 export default function RootLayout() {
