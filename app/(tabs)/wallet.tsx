2bf9784c2e34d1f987eee251fa6eb57bf6e079cf
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2025-05-03 11:12:23 +0600

    feat(wallet): add UI components for wallet screen

--- app/(tabs)/wallet.tsx
+++ app/(tabs)/wallet.tsx
@@ -1,15 +1,80 @@
 import ScreenWrapper from '@/components/ScreenWrapper'
+import { colors, radius, spacingX, spacingY } from '@/constants/theme'
+import { verticalScale } from '@/utils/styling'
+import { useRouter } from 'expo-router'
+import { PlusCircle } from 'phosphor-react-native'
 import React from 'react'
-import { StyleSheet, Text } from 'react-native'
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import Typo from '../../components/Typo'
 
 const Wallet = () => {
+  const router = useRouter()
+  const getTotalBalance = () => {
+    return 2344
+  }
   return (
-    <ScreenWrapper>
-      <Text>Wallet</Text>
+    <ScreenWrapper style={{ backgroundColor: colors.black }}>
+      <View style={styles.container}>
+        <View style={styles.balanceView}>
+          <View style={{ alignItems: 'center' }}>
+            <Typo size={45} fontWeight={'500'}>
+              ${getTotalBalance()?.toFixed(2)}
+            </Typo>
+            <Typo size={16} color={colors.neutral300}>
+              Total Balance
+            </Typo>
+          </View>
+        </View>
+
+        <View style={styles.wallets}>
+          <View style={styles.flexRow}>
+            <Typo size={20} fontWeight={'500'}>
+              My Wallets
+            </Typo>
+            <TouchableOpacity
+              onPress={() => router.push('/(modals)/walletModal')}>
+              <PlusCircle
+                weight='fill'
+                color={colors.primary}
+                size={verticalScale(33)}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </ScreenWrapper>
   )
 }
 
 export default Wallet
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  balanceView: {
+    height: verticalScale(160),
+    backgroundColor: colors.black,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  flexRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: spacingY._10
+  },
+  wallets: {
+    flex: 1,
+    backgroundColor: colors.neutral900,
+    borderTopRightRadius: radius._30,
+    borderTopLeftRadius: radius._30,
+    padding: spacingX._20,
+    paddingTop: spacingY._25
+  },
+  listStyle: {
+    paddingVertical: spacingY._10,
+    paddingTop: spacingY._15
+  }
+})
