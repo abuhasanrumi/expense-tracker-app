b6cf42970ccd3f44343211430609856262e61415
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2025-05-04 02:48:03 +0600

    feat(walletService): add createOrUpdateWallet function

---
+++ services/walletService.ts
@@ -0,0 +1,42 @@
+import { firestore } from '@/config/firebase'
+import { ResponseType, WalletType } from '@/types'
+import { collection, doc, setDoc } from 'firebase/firestore'
+import { uploadFileToCloudinary } from './imageService'
+
+export const createOrUpdateWallet = async (
+  walletData: Partial<WalletType>
+): Promise<ResponseType> => {
+  try {
+    let walletToSave = { ...walletData }
+    if (walletData.image) {
+      const imageUploadRes = await uploadFileToCloudinary(
+        walletData.image,
+        'wallets'
+      )
+      if (!imageUploadRes.success) {
+        return {
+          success: false,
+          message: imageUploadRes.message || 'Failed to upload image'
+        }
+      }
+      walletToSave.image = imageUploadRes.data
+    }
+
+    if (!walletData.id) {
+      walletToSave.amount = 0
+      walletToSave.totalIncome = 0
+      walletToSave.totalExpense = 0
+      walletToSave.created = new Date()
+    }
+
+    const walletRef = walletData?.id
+      ? doc(firestore, 'wallets', walletData?.id)
+      : doc(collection(firestore, 'wallets'))
+
+    await setDoc(walletRef, walletToSave, { merge: true })
+    return { success: true, data: { ...walletToSave, id: walletRef.id } }
+  } catch (error) {
+    console.log('Error creating or updating wallet', error)
+    return { success: false, message: 'Failed to create or update wallet' }
+  }
+}
