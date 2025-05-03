49e7bb26a0a92a19aa79cfb3411380a25c6b58b0
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2025-05-03 11:12:09 +0600

    feat(wallet): add wallet modal for creating new wallets

---
+++ app/(modals)/walletModal.tsx
@@ -0,0 +1,140 @@
+import BackButton from '@/components/BackButton'
+import Button from '@/components/Button'
+import Header from '@/components/Header'
+import Input from '@/components/Input'
+import ModalWrapper from '@/components/ModalWrapper'
+import Typo from '@/components/Typo'
+import { colors, spacingX, spacingY } from '@/constants/theme'
+import { useAuth } from '@/contexts/authContext'
+import { updateUser } from '@/services/userService'
+import { WalletType } from '@/types'
+import { scale, verticalScale } from '@/utils/styling'
+import * as ImagePicker from 'expo-image-picker'
+import { useRouter } from 'expo-router'
+import React, { useState } from 'react'
+import { Alert, ScrollView, StyleSheet, View } from 'react-native'
+
+const WalletModal = () => {
+  const { user, updateUserData } = useAuth()
+  const [wallet, setWallet] = useState<WalletType>({
+    name: '',
+    image: null
+  })
+  const router = useRouter()
+
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const onSubmit = async () => {
+    let { name, image } = wallet
+    if (!name.trim()) {
+      Alert.alert('User', 'Please fill all the fields.')
+      return
+    }
+    setLoading(true)
+    const res = await updateUser(user?.uid as string, wallet)
+    setLoading(false)
+    if (res.success) {
+      updateUserData(user?.uid as string)
+      router.back()
+    } else Alert.alert('User', res.msg)
+  }
+
+  const onPickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      aspect: [4, 3],
+      quality: 0.5
+    })
+
+    if (!result.canceled) {
+      // setUserData({ ...userData, image: result.assets[0] })
+    }
+  }
+  return (
+    <ModalWrapper>
+      <View style={styles.container}>
+        <Header
+          title='New Wallet'
+          leftIcon={<BackButton />}
+          style={{ marginBottom: spacingY._10 }}
+        />
+
+        <ScrollView contentContainerStyle={styles.form}>
+          <View style={styles.inputContainer}>
+            <Typo color={colors.neutral200}>Wallet Name</Typo>
+            <Input
+              placeholder='Salary'
+              value={wallet.name}
+              onChangeText={(value) => setWallet({ ...wallet, name: value })}
+            />
+          </View>
+        </ScrollView>
+      </View>
+
+      <View style={styles.footer}>
+        <Button onPress={onSubmit} loading={loading} style={{ flex: 1 }}>
+          <Typo color={colors.black} fontWeight={'700'}>
+            Add Wallet
+          </Typo>
+        </Button>
+      </View>
+    </ModalWrapper>
+  )
+}
+
+export default WalletModal
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    paddingHorizontal: spacingX._20
+  },
+  footer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingHorizontal: spacingX._20,
+    gap: scale(12),
+    paddingTop: spacingY._15,
+    borderTopColor: colors.neutral700,
+    marginBottom: spacingY._5,
+    borderTopWidth: 1
+  },
+  form: {
+    gap: spacingY._30,
+    marginTop: spacingY._15
+  },
+  avatarContainer: {
+    position: 'relative',
+    alignSelf: 'center'
+  },
+  avatar: {
+    alignSelf: 'center',
+    backgroundColor: colors.neutral300,
+    height: verticalScale(135),
+    width: verticalScale(135),
+    borderRadius: 200,
+    borderWidth: 1,
+    borderColor: colors.neutral500
+  },
+  editIcon: {
+    position: 'absolute',
+    bottom: spacingY._5,
+    right: spacingY._7,
+    borderRadius: 100,
+    backgroundColor: colors.neutral100,
+    shadowColor: colors.black,
+    shadowOffset: {
+      width: 0,
+      height: 0
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+    elevation: 4,
+    padding: spacingY._7
+  },
+  inputContainer: {
+    gap: spacingY._10
+  }
+})
