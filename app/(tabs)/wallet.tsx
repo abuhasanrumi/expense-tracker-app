e6c466b7cb4261e54341cbda481cdb4666332fd2
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2025-05-04 19:14:38 +0600

    feat(wallet): add wallet list and loading state

--- app/(tabs)/wallet.tsx
+++ app/(tabs)/wallet.tsx
@@ -1,17 +1,35 @@
+import Loading from '@/components/Loading'
 import ScreenWrapper from '@/components/ScreenWrapper'
+import WalletListItem from '@/components/WalletListItem'
 import { colors, radius, spacingX, spacingY } from '@/constants/theme'
+import { useAuth } from '@/contexts/authContext'
+import useFetchData from '@/hooks/useFetchData'
+import { WalletType } from '@/types'
 import { verticalScale } from '@/utils/styling'
 import { useRouter } from 'expo-router'
+import { orderBy, where } from 'firebase/firestore'
 import { PlusCircle } from 'phosphor-react-native'
 import React from 'react'
-import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import { FlatList, StyleSheet, TouchableOpacity, View } from 'react-native'
 import Typo from '../../components/Typo'
 
 const Wallet = () => {
   const router = useRouter()
-  const getTotalBalance = () => {
-    return 2344
-  }
+  const { user } = useAuth()
+
+  const {
+    data: wallets,
+    error,
+    loading
+  } = useFetchData<WalletType>('wallets', [
+    where('uid', '==', user?.uid),
+    orderBy('created', 'desc')
+  ])
+
+  const getTotalBalance = () =>
+    wallets.reduce((total, item) => {
+      return total + (item.amount || 0)
+    }, 0)
   return (
     <ScreenWrapper style={{ backgroundColor: colors.black }}>
       <View style={styles.container}>
@@ -40,6 +58,17 @@ const Wallet = () => {
               />
             </TouchableOpacity>
           </View>
+
+          {loading && <Loading />}
+          <FlatList
+            data={wallets}
+            renderItem={({ item, index }) => {
+              return (
+                <WalletListItem item={item} index={index} router={router} />
+              )
+            }}
+            contentContainerStyle={styles.listStyle}
+          />
         </View>
       </View>
     </ScreenWrapper>
