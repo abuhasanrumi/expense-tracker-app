d15aa5f9b7cf1ec673e7fbeb468218aab5921485
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2025-05-04 19:15:36 +0600

    feat(hooks): add useFetchData hook for fetching Firestore data

---
+++ hooks/useFetchData.ts
@@ -0,0 +1,43 @@
+import { firestore } from '@/config/firebase'
+import { collection, onSnapshot, query } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+import { StyleSheet } from 'react-native'
+
+const useFetchData = <T>(
+  collectionName: string,
+  constraints: QueryConstraint[] = []
+) => {
+  const [data, setData] = useState<T[]>([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    if (!collectionName) return
+    const collectionRef = collection(firestore, collectionName)
+    const q = query(collectionRef, ...constraints)
+
+    const unsub = onSnapshot(
+      q,
+      (snapshot) => {
+        const fetchedData = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data()
+        })) as T[]
+        setData(fetchedData)
+        setLoading(false)
+        setError(null)
+      },
+      (err) => {
+        console.log('Error fetching data: ', err.message)
+        setError(err.message)
+        setLoading(false)
+      }
+    )
+    return () => unsub()
+  }, [])
+  return { data, loading, error }
+}
+
+export default useFetchData
+
+const styles = StyleSheet.create({})
