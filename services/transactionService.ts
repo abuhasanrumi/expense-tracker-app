a84b277d135be21cfea3e642559690d742aea746
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2025-05-14 23:58:27 +0600

    feat(transactionService): implement transaction creation with image upload and wallet update

--- services/transactionService.ts
+++ services/transactionService.ts
@@ -1,6 +1,7 @@
 import { firestore } from '@/config/firebase'
 import { TransactionType, WalletType } from '@/types'
-import { doc, getDoc, updateDoc } from 'firebase/firestore'
+import { collection, doc, getDoc, setDoc, updateDoc } from 'firebase/firestore'
+import { uploadFileToCloudinary } from './imageService'
 
 export const createOrUpdateTransaction = async (
   transactionData: Partial<TransactionType>
@@ -17,8 +18,40 @@ export const createOrUpdateTransaction = async (
     if (id) {
       // todo: update existing transaction
     } else {
-      // todo: create new transaction
-      // update wallet
+      let res = await updateWalletForNewTransaction(
+        walletId!,
+        Number(amount!),
+        type
+      )
+      if (!res.success) {
+        return res
+      }
+
+      if (image) {
+        // todo: upload image
+        const imageUploadRes = await uploadFileToCloudinary(
+          image,
+          'transactions'
+        )
+        if (!imageUploadRes.success) {
+          return {
+            success: false,
+            msg: "Couldn't upload image"
+          }
+        }
+        transactionData.image = imageUploadRes.data
+      }
+
+      const transactionRef = id
+        ? doc(firestore, 'transactions', id)
+        : doc(collection(firestore, 'transactions'))
+
+      await setDoc(transactionRef, transactionData, { merge: true })
+      return {
+        success: true,
+        data: { ...transactionData, id: transactionRef.id }
+      }
+    }
   } catch (err: any) {
     console.log('Error creating or updating transaction: ', err)
     return {
@@ -38,27 +71,33 @@ const updateWalletForNewTransaction = async (
     const walletRef = doc(firestore, 'wallets', walletId)
     const walletSnapshot = await getDoc(walletRef)
     if (!walletSnapshot.exists()) {
-      console.log("Wallet not found")
+      console.log('Wallet not found')
       return {
         success: false,
         msg: 'Wallet not found'
       }
     }
-    
+
     const walletData = walletSnapshot.data() as WalletType
 
-    if(type == 'expense' && walletData.amount! - amount < 0) {
-      console.log("Insufficient balance")
+    if (type == 'expense' && walletData.amount! - amount < 0) {
+      console.log('Insufficient balance')
       return {
         success: false,
         msg: 'Insufficient balance'
       }
     }
 
-    const updateType = type == 'income' ? "totalIncome" : "totalExpense"
-    const updatedWalletAmount = type == 'income' ? Number(walletData.amount) + amount : Number(walletData.amount) - amount
-    
-    const updatedTotals = type == 'income' ? Number(walletData.totalIncome) + amount : Number(walletData.totalExpense) + amount
+    const updateType = type == 'income' ? 'totalIncome' : 'totalExpense'
+    const updatedWalletAmount =
+      type == 'income'
+        ? Number(walletData.amount) + amount
+        : Number(walletData.amount) - amount
+
+    const updatedTotals =
+      type == 'income'
+        ? Number(walletData.totalIncome) + amount
+        : Number(walletData.totalExpense) + amount
 
     await updateDoc(walletRef, {
       amount: updatedWalletAmount,
