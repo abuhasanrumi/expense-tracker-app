5abdfdc002e5becf0b7ebad9f03afcf8ba3b7839
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2025-05-18 12:49:21 +0600

    feat(walletService): add transaction cleanup when deleting wallet

--- services/walletService.ts
+++ services/walletService.ts
@@ -1,6 +1,15 @@
 import { firestore } from '@/config/firebase'
 import { ResponseType, WalletType } from '@/types'
-import { collection, deleteDoc, doc, setDoc } from 'firebase/firestore'
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  query,
+  setDoc,
+  where,
+  writeBatch
+} from 'firebase/firestore'
 import { uploadFileToCloudinary } from './imageService'
 
 export const createOrUpdateWallet = async (
@@ -45,6 +54,44 @@ export const deleteWallet = async (walletId: string): Promise<ResponseType> => {
   try {
     const walletRef = doc(firestore, 'wallets', walletId)
     await deleteDoc(walletRef)
+    deleteTrxByWalletId(walletId)
+    return { success: true, message: 'Wallet deleted successfully' }
+  } catch (error) {
+    console.log('Error deleting wallet', error)
+    return { success: false, message: 'Failed to delete wallet' }
+  }
+}
+
+export const deleteTrxByWalletId = async (
+  walletId: string
+): Promise<ResponseType> => {
+  try {
+    let hasMoreTransactions = true
+
+    while (hasMoreTransactions) {
+      const transactionQuery = query(
+        collection(firestore, 'transactions'),
+        where('walletId', '==', walletId)
+      )
+
+      const transactionSnapshot = await getDocs(transactionQuery)
+      if (transactionSnapshot.size == 0) {
+        hasMoreTransactions = false
+        break
+      }
+
+      const batch = writeBatch(firestore)
+      transactionSnapshot.forEach((transactionDoc) => {
+        batch.delete(transactionDoc.ref)
+      })
+
+      await batch.commit()
+
+      console.log(
+        `${transactionSnapshot.size} transactions deleted from wallet ${walletId}`
+      )
+    }
+
     return { success: true, message: 'Wallet deleted successfully' }
   } catch (error) {
     console.log('Error deleting wallet', error)
